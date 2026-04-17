1db4dac0575ce33b94da57d1181eb7c53de7e646
Nicksheinman <nicksheinman@gmail.com>
2026-04-17 20:46:25 +0300

    changesForProduction3

--- backend/url_shortener/mail/send_email.py
+++ backend/url_shortener/mail/send_email.py
@@ -3,16 +3,19 @@ from ..models import EmailVertification, PasswordVertification
 from django.core.mail import send_mail
 import logging 
 logger = logging.getLogger(__name__)
+import os
 
 def send_email(user):
     token=str(uuid.uuid4())
     EmailVertification.objects.create(user=user,token=token)
-
+    frontend_url = os.getenv("FRONTEND_URL", "http://127.0.0.1:5173")
+    confirm_link = f"{frontend_url}/email_confirm?token={token}"
+    
     try:
 
         send_mail(
             subject='email vertification',
-            message=f'hello {user.username}, this is your link to vertification: http://127.0.0.1:5173/email_confirm?token={token}',
+            message=f'hello {user.username}, this is your link to vertification: {confirm_link}',
             from_email='noreply@url_short.com',
             recipient_list=[user.email],
             fail_silently=False
@@ -25,14 +28,16 @@ def send_email(user):
 
 def send_email_password(user):
     token=str(uuid.uuid4())
-    print(1)
     PasswordVertification.objects.update_or_create(user=user,defaults={'token':token})
-    print(2)
+    EmailVertification.objects.create(user=user,token=token)
+    frontend_url = os.getenv("FRONTEND_URL", "http://127.0.0.1:5173")
+    confirm_link = f"{frontend_url}/email_confirm?token={token}"
+    
     try:
 
         send_mail(
             subject='password change',
-            message=f'hello {user.username}, this is your link to password change: http://127.0.0.1:5173/newPassword?token={token}',
+            message=f'hello {user.username}, this is your link to password change:{confirm_link}',
             from_email='noreply@url_short.com',
             recipient_list=[user.email],
             fail_silently=False

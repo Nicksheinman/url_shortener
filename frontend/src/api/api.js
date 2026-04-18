9910a9afc0e1eb140d23970b39010d6c281583c7
Nicksheinman <nicksheinman@gmail.com>
2026-04-18 21:28:46 +0300

    changesForProduction10

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API } from "./client";
 
 const api = axios.create({
-  baseURL: `${API}/api`,
+  baseURL: `${API}`,
   withCredentials: true,           
   xsrfCookieName: "csrftoken",  
   xsrfHeaderName: "X-CSRFToken",          

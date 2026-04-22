d927431ede3bcf2bcc5f05ea20c7cb803ba068da
Nicksheinman <nicksheinman@gmail.com>
2026-04-22 20:22:16 +0300

    changesForProduction13

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,4 +8,5 @@ const api = axios.create({
   xsrfHeaderName: "X-CSRFToken",          
 });
 
+
 export default api
--- frontend/src/api/auth/csrf.js
+++ frontend/src/api/auth/csrf.js
@@ -1,8 +1,21 @@
 import api from "../api"
 
-const getSCRF=()=>{
-    api.get('/csrf/')
-}
+let csrfToken = null;
+
+export const getSCRF = async () => {
+  const response = await api.get("/csrf/");
+  csrfToken = response.data.csrfToken;
+};
+
+api.interceptors.request.use((config) => {
+  const methods = ["post", "put", "patch", "delete"];
+
+  if (csrfToken && methods.includes(config.method)) {
+    config.headers["X-CSRFToken"] = csrfToken;
+  }
+
+  return config;
+});
 
 
 

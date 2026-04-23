f8d7a69088eaef9e99a6e817a0ad4243241a65b7
Nicksheinman <nicksheinman@gmail.com>
2026-04-23 22:01:15 +0300

    changesForProduction20

--- frontend/src/api/user/passwordChangeEmail.js
+++ frontend/src/api/user/passwordChangeEmail.js
@@ -1,6 +1,7 @@
 import api from "../api";
-
+import getSCRF from "../auth/csrf";
 const passwordChangeEmail=async (email)=>{
+    await getSCRF()
     await api.post('password_email/', {email:email})
     return true
 }
